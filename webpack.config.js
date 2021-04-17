27d1da9d3b689c5665a1a092b39fce050738fc37
Denis Lafontant <icebox827@gmail.com>
2021-04-17 18:34:33 -0400

    Config phaser

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -14,7 +18,23 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
-      }
+      },
+    ],
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      }),
+      new CopyWebpackPlugin([
+        {
+          from: path.resolve(__dirname, 'index.html'),
+          to: path.resolve(__dirname, 'build')
+        }
+      ]),
+      new webpack.DefinePlugin({
+        'typeof CANVAS_RENDERER': JSON.stringify(true),
+        'typeof WEBGL_RENDERER': JSON.stringify(true)
+      })
     ]
   },
 };
