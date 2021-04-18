bb4390aa6e4bf823d307005689c0042a7f954a24
Denis Lafontant <icebox827@gmail.com>
2021-04-17 20:59:18 -0400

    Update title

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 import Preload from './scene/preload';
 import Game from './scene/game';
+import Title from './scene/title'
 
 window.onload = function() {
   const config = {
@@ -10,6 +11,10 @@ window.onload = function() {
     scene: {
       Preload,
       Game,
+      Title
+    },
+    dom: {
+      createContainer: true,
     },
     backgroundColor: 0x444484,
     physics: {
@@ -18,22 +23,4 @@ window.onload = function() {
   }
   const game = new Phaser.Game(config);
   window.focus();
-  resize();
-  window.addEventListener("resize", resize, false);
-
-  function resize(){
-    let canvas = document.querySelector("canvas");
-    let windowWidth = window.innerWidth;
-    let windowHeight = window.innerHeight;
-    let windowRatio = windowWidth / windowHeight;
-    let gameRatio = game.config.width / game.config.height;
-    if(windowRatio < gameRatio){
-        canvas.style.width = windowWidth + "px";
-        canvas.style.height = (windowWidth / gameRatio) + "px";
-    }
-    else{
-        canvas.style.width = (windowHeight * gameRatio) + "px";
-        canvas.style.height = windowHeight + "px";
-    }
-  }
 }
--- src/scene/title.js
+++ src/scene/title.js
@@ -1,8 +1,39 @@
 import Phaser from 'phaser';
+import gameState from './boot';
 
 class Title extends Phaser.Scene {
   constructor() {
     super('Title')
+  }
+
+  create() {
+    this.add.text(160, 50, 'Damian Rush', { fontSize: '48px', fill: 'Blue', fontFamily: 'bold' });
+    this.add.text(207, 120, 'Enter your name', { fontSize: '22px', fill: 'green' });
+ 
+    const input = document.createElement('input');
+    const btn = document.createElement('button');
+    
+    input.id = 'input';
+    btn.id = 'btn';
+    btn.type = 'button';
+    btn.textContent = 'Start';
+
+    document.body.appendChild(input);
+    document.body.appendChild(btn);
+  }
+
+  update(){
+    document.getElementById('btn').onclick = () => {
+      if (document.getElementById('input').value !== '') {
+        this.name = document.getElementById('input').value;
+        Cred.name = this.name;
+        this.scene.start('play');
+        document.getElementById('input').remove();
+        document.getElementById('btn').remove();
+      } else {
+        this.add.text(90, 320, 'Please enter your name!', { fontSize: '22px', fill: 'red' });
+      }
+    };
   }
 }
 
