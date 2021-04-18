aa69d9e44c25144919c59a3c27757c4d7b401fcf
Denis Lafontant <icebox827@gmail.com>
2021-04-17 20:38:36 -0400

    Update API

--- src/api.js
+++ src/api.js
@@ -1,10 +1,20 @@
-const api = async() => {
-  const key = 'Zl4d7IVkemOTTVg2fUdz';
-  const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${key}/score`;
+const key = 'Zl4d7IVkemOTTVg2fUdz';
+const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${key}/score`;
 
-  const response = await fetch(url, { mode: 'cors' }).then(e => e.json())
+const getScore = async() => {
+  const response = await fetch(url, { mode: 'cors' }).then(e => e.json());
 
-  return response
+  return response;
 }; 
 
-export default api;
+const setScore = async() => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ user: name, score }),
+  });
+
+  return response;
+};
+
+export default {getScore, setScore};
