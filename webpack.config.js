fa335609b358a13ca9d617ea5ddb861bc6ebf8be
Denis Lafontant <icebox827@gmail.com>
2021-04-19 18:11:50 -0400

    Update Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,4 @@
 const path = require('path');
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const CopyWebpackPlugin = require("copy-webpack-plugin");
-// const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -25,21 +21,5 @@ module.exports = {
         }
       },
     ],
-    // plugins: [
-    //   new MiniCssExtractPlugin({
-    //     filename: "[name].css",
-    //     chunkFilename: "[id].css"
-    //   }),
-    //   new CopyWebpackPlugin([
-    //     {
-    //       from: path.resolve(__dirname, 'index.html'),
-    //       to: path.resolve(__dirname, 'dist')
-    //     }
-    //   ]),
-    //   new webpack.DefinePlugin({
-    //     'typeof CANVAS_RENDERER': JSON.stringify(true),
-    //     'typeof WEBGL_RENDERER': JSON.stringify(true)
-    //   })
-    // ]
   },
 };
