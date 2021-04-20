e32b8b8c982dd6fe93bfabff303edb2aa1ec86d6
Denis Lafontant <icebox827@gmail.com>
2021-04-20 19:13:04 -0400

    Update score display

--- src/scene/gameOver.js
+++ src/scene/gameOver.js
@@ -33,8 +33,8 @@ class GameOver extends Phaser.Scene {
         this.add.text(50, 40, 'Leaderboard Score :');
         this.add.text(50, 100 + (i * 50), `${scores[i].user}'s Score is ${scores[i].score}`);
       }
-      this.add.text(550, 50, 'Your Score :');
-      this.add.text(550, 90, gameState.score);
+      this.add.text(500, 50, 'Your Score :');
+      this.add.text(500, 90, gameState.score);
       if (gameState.score !== 0) {
         api.setScore(gameState.name, gameState.score);
       }
