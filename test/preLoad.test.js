2dd63bf74f36a99ce805f78376b78699ae077380
Denis Lafontant <icebox827@gmail.com>
2021-04-21 17:07:31 -0400

    Add negative scenarios TSe changes

--- test/preLoad.test.js
+++ test/preLoad.test.js
@@ -1,9 +1,14 @@
 import '@babel/polyfill';
 import 'regenerator-runtime/runtime';
-
 import Phaser from 'phaser';
 import Preload from '../src/scene/game';
 
+const music = require('../src/scene/preload');
+
 it('should be a subclass of Phaser', () => {
   expect(Preload.prototype instanceof Phaser.Scene).toBe(true);
+});
+
+it('should not be a subclass of Phaser', () => {
+  expect(music.prototype instanceof Phaser.Scene).toBe(false);
 });
