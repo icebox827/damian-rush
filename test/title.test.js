a0061d4029713fa4805d5101a8dbcf6ab57b264f
Denis Lafontant <icebox827@gmail.com>
2021-04-21 14:10:49 -0400

    Add test for Title

---
+++ test/title.test.js
@@ -0,0 +1,9 @@
+import '@babel/polyfill';
+import 'regenerator-runtime/runtime';
+
+import Phaser from 'phaser';
+import Title from '../src/scene/game';
+
+it('should be a subclass of Phaser', () => {
+  expect(Title.prototype instanceof Phaser.Scene).toBe(true);
+});
