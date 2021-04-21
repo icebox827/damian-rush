e5b8c5155af736f658c892100e31da16d61e88ef
Denis Lafontant <icebox827@gmail.com>
2021-04-21 16:24:44 -0400

    Test negative scenarios TSE changes

--- test/gameOver.test.js
+++ test/gameOver.test.js
@@ -1,9 +1,14 @@
 import '@babel/polyfill';
 import 'regenerator-runtime/runtime';
-
 import Phaser from 'phaser';
 import GameOver from '../src/scene/game';
 
+const result = require('../src/scene/gameOver');
+
 it('should be a subclass of Phaser', () => {
   expect(GameOver.prototype instanceof Phaser.Scene).toBe(true);
+});
+
+it('should not be a subclass of Phaser', () => {
+  expect(result.prototype instanceof Phaser.Scene).toBe(false);
 });
