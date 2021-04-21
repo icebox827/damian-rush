a650f79607c3402d9d2ef6daadc47e44b3f10c36
Denis Lafontant <icebox827@gmail.com>
2021-04-21 17:18:44 -0400

    Elaborate test TSE changes

--- test/boot.test.js
+++ test/boot.test.js
@@ -1,5 +1,14 @@
+import 'regenerator-runtime/runtime';
+
 const gameState = require('../src/scene/boot');
+const score = require('../src/scene/boot');
 
 it('should create object', () => {
   expect(typeof gameState).toBe('object');
+});
+
+it('should increase score', () => {
+  if (gameState.score > score) {
+    expect(gameState.score).toBe(true);
+  }
 });
